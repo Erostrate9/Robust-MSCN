ba795f1977c7b4c1b8b5d80390f087d7972b4b2a
Erostrate9 <ericsun42@outlook.com>
2024-08-26 22:46:50 +0000

    Update notebook

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import wandb
 import logging
 logger = logging.getLogger("wandb")
 logger.setLevel(logging.ERROR)
+import time
 
 def eval_alg(alg, eval_funcs, qreps, cfg,
         samples_type,
@@ -228,13 +229,22 @@ def main():
         alg.train(trainqs, valqs=valqs, testqs=None, evalqs = None,
                 eval_qdirs = eval_qdirs, featurizer=featurizer)
 
+    start_time = time.time()
     eval_alg(alg, eval_fns, trainqs, cfg, "train", featurizer=featurizer)
+    execution_time = time.time() - start_time
+    print(f"{args.arg} evaluation time on train set: {execution_time:.2f} seconds")
 
     if len(valqs) > 0:
+        start_time = time.time()
         eval_alg(alg, eval_fns, valqs, cfg, "val", featurizer=featurizer)
+        execution_time = time.time() - start_time
+        print(f"{args.arg} evaluation time on val set: {execution_time:.2f} seconds")
 
     if len(testqs) > 0:
+        start_time = time.time()
         eval_alg(alg, eval_fns, testqs, cfg, "test", featurizer=featurizer)
+        execution_time = time.time() - start_time
+        print(f"{args.arg} evaluation time on test set: {execution_time:.2f} seconds")
 
     if len(evalqs) > 0 and len(evalqs[0]) > 0:
         for ei, evalq in enumerate(evalqs):
