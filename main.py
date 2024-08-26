befba8498b30e319412ec34e47fc0e37e6014d79
Erostrate9 <ericsun42@outlook.com>
2024-08-26 20:30:03 +0000

    Update notebook

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ def read_flags():
     parser.add_argument("--db_host", type=str, required=False,
             default="localhost")
     parser.add_argument("--user", type=str, required=False,
-            default="ceb")
+            default="imdb")
     parser.add_argument("--pwd", type=str, required=False,
             default="password")
     parser.add_argument("--port", type=int, required=False,
